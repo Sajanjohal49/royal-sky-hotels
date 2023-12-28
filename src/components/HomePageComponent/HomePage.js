67b74ff6000e13b38f32df2860c5d9526404c43f
Sajandeep Singh <sajanjohal49@gmail.com>
2023-12-28 18:40:42 -0500

    layout Fix

--- src/components/HomePageComponent/HomePage.js
+++ src/components/HomePageComponent/HomePage.js
@@ -12,8 +12,8 @@ import GetAllLocations from "../LocationComponent/GetAllLocations";
 
 function HomePage() {
   return (
-    <div className=" font-euclidRegular max-w-[1500px]">
-      <div className="relative overflow-hidden bg-[#F3F0EA] sm:px-2 lg:px-6 dark:bg-gray-900">
+    <div className=" font-euclidRegular  bg mx-auto  ">
+      <div className="relative overflow-hidden bg-[#F3F0EA] sm:px-2 lg:px-6 dark:bg-gray-900 ">
         <VideoAndText />
         <GetAllLocations />
         <HotelsGrid />
